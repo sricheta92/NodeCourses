302ae5860907433d74548dad54aeec472faadcf9
Sricheta Ruj <srichetaruj.iste@gmail.com>
2017-10-28 17:38:47 +0530

    Initial commit

---
+++ HelloNode.js
@@ -0,0 +1,6 @@
+var name = "Sricheta"
+var date1 = new Date();
+var date2 = new Date(2022,11,25);
+console.log(date1.getYear());
+console.log(date2.getYear());
+console.log(name+" "+date2.getYear()-date1.getYear());
